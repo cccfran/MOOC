4d4db79d1902f77d812928c9555aa27a8c6ab575
cccfranco <flashcreate4@126.com>
2015-06-15 12:24:29 +0800

    PS2

---
+++ Udacity/Data-Wrangling-with-MongoDB/Lesson2/airports.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# All your changes should be in the 'extract_airports' function
+# It should return a list of airport codes, excluding any combinations like "All"
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html)
+        list = soup.find(id = 'AirportList')
+        for option in list.find_all('option'):
+        	name = option["value"]
+        	if 'All' not in name:
+        		data.append(name)
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+test()
